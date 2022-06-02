e71d8852bb0107b15ae826dec475a3cb70d897e6
Javier Orjuela <101306063+jorjuela575@users.noreply.github.com>
2022-06-01 21:39:17 -0500

    Add files via upload
    my web porfolio

---
+++ Script-webpage.js
@@ -0,0 +1,13 @@
+/*=======================type animation==================================*/
+var typed = new Typed(".typing",{
+    Strings:["Industrial Engineer","Ms. International Business","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
+var typed2 = new Typed(".CV",{
+    Strings:["Industrial Engineer","Ms. International Business","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    
+})
